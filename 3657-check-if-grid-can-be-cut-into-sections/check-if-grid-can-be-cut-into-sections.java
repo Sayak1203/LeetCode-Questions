86b12f1eeeb4d63a95bcda2db9a34e9f6b3820af
Sayak1203 <123932629+Sayak1203@users.noreply.github.com>
2025-03-25 19:43:42 +0530

    Time: 118 ms (28.95%) | Memory: 122.3 MB (57.46%) - LeetSync

---
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.java
@@ -0,0 +1,30 @@
+class Solution {
+    static boolean solve(int[][] x){
+        Arrays.sort(x,(a,b)->(a[0]-b[0]));
+        int end = x[0][1];
+        int n = x.length;
+        int ans=0;
+        for(int i=1; i<n; i++){
+            if(x[i][0]>=end){
+                ans++;
+                end=x[i][1];
+            }
+            else{
+                end=Math.max(end,x[i][1]);
+            }
+        }
+        return ans>=2;
+    }
+    public boolean checkValidCuts(int n, int[][] rec) {
+        int m=rec.length;
+        int[][] x = new int[m][2];
+        int[][] y = new int[m][2];
+        for(int i=0; i<m; i++){
+            x[i][0]=rec[i][0];
+            x[i][1]=rec[i][2];
+            y[i][0]=rec[i][1];
+            y[i][1]=rec[i][3];
+        }
+        return solve(x)|solve(y);        
+    }
+}
