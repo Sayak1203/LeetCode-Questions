c9843321704221947f1c777bff056aba7487fc3c
Sayak1203 <123932629+Sayak1203@users.noreply.github.com>
2025-09-11 09:21:19 +0530

    Time: 1 ms (3.59%) | Memory: 42.6 MB (14.89%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,92 +1,60 @@
 class Solution {
-    static void streverse(int[] st, int n){
-        int l=0;
-        n--;
-        while(l<n){
-            int tem=st[l];
-            st[l]=st[n];
-            st[n]=tem;
-            l++;
-            n--;
-        }
-        
-    }
-    static int[] getStr(int[][] g, int i){
+    static Stack<Integer> getStr1(int[][] mat, int i, int m, int n){
         int j=0;
-        int n = g.length;
-        int[] st = new int[n];
-        while(i<g.length){
-            st[j]=g[i][j];
-            i++;
+        Stack<Integer> st = new Stack<>();
+        while(j<n && i<m){
+            st.push(mat[j][i]);
             j++;
+            i++;
         }
         return st;
     }
-    void putStr(int[][] g, int i, int[] st){
+    static void putStr1(int [][] mat, int i, Stack<Integer> st, int m, int n){
         int j=0;
-        while(i<g.length){
-            g[i][j]=st[j];
-            i++;
+        while(j<n && i<m){
+            mat[j][i]= st.pop();
             j++;
+            i++;
         }
     }
-    static int[] getStr1(int[][] g, int i){
+
+    static Stack<Integer> getStr2(int[][] mat, int i, int m, int n){
         int j=0;
-        int n = g.length;
-        int[] st = new int[n];
-        while(i<g.length){
-            st[j]=g[j][i];
-            i++;
+        Stack<Integer> st = new Stack<>();
+        while(j<m && i<n){
+            st.push(mat[i][j]);
             j++;
+            i++;
         }
         return st;
     }
-    void putStr1(int[][] g, int i, int[] st){
+    static void putStr2(int [][] mat, int i, Stack<Integer> st, int m, int n){
         int j=0;
-        while(i<g.length){
-            g[j][i]=st[j];
+        while(j<m && i<n){
+            mat[i][j]= st.pop();
+            j++;
             i++;
-            j++;
         }
     }
-    static int[] getStr2(int[][] g, int i){
-        int j=0;
-        int n = g.length;
-        int[] st = new int[n];
-        while(j<g.length){
-            st[j]=g[j][i];
-            j++;
-        }
-        return st;
-    }
-    void putStr2(int[][] g, int i, int[] st){
-        int j=0;
-        while(j<g.length){
-            g[j][i]=st[j];
-            j++;
-        }
-    }
-    public void rotate(int[][] g) {
-        int n = g.length;
+
+    public void rotate(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+
         for(int i=0; i<n; i++){
-            int[] st = getStr(g, i);
-            streverse(st, n-i);
-            putStr(g, i, st);
+            for(int j=0; j<m/2; j++){
+                int tem = mat[i][j];
+                mat[i][j] = mat[i][m-j-1];
+                mat[i][m-j-1]=tem;
+            }
         }
-        for(int i=1; i<n; i++){
-            int[] st = getStr1(g, i);
-            streverse(st, n-i);
-            putStr1(g, i, st);
+        for(int i=m-2; i>=0; i--){
+            Stack<Integer> st = getStr1(mat, i, m, n);
+            putStr1(mat, i, st, m, n);
         }
-        // for(int[] i: g)
-        // System.out.println(Arrays.toString(i));
-        for(int i=0; i<n; i++){
-            int[] st = getStr2(g, i);
-            streverse(st, n);
-            putStr2(g, i, st);
+        for(int i=n-2; i>0; i--){
+            Stack<Integer> st = getStr2(mat, i, m, n);
+            putStr2(mat, i, st, m, n);
         }
-        // for(int[] i: g)
-        // System.out.println(Arrays.toString(i));
-        
     }
 }
