b5d0e25a75fdb13f66fdd3b9806ccadbd1612376
Sayak1203 <123932629+Sayak1203@users.noreply.github.com>
2025-10-28 18:49:43 +0530

    Time: 430 ms (5.01%) | Memory: 45.8 MB (6.51%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -3,21 +3,31 @@ class Solution {
         int n = a.length;
         List<List<Integer>> ar = new ArrayList<>();
         HashSet<String> set = new HashSet<>();
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
+        HashSet<Long> map = new HashSet<>();
+        // for(int i=0; i<n; i++){
+            map.add((long)a[0]);
+            for(int j=1; j<n; j++){
                 for(int k=j+1; k<n; k++){
                     for(int l=k+1; l<n; l++){
-                        if((long)a[i]+(long)a[j]+(long)a[k]+(long)a[l]==(long)tar){
+                        int first=-1;
+                        // System.out.println(a[j]+" "+a[k]+" "+a[l]+" "+map);
+                        if(map.contains((long)tar-(long)a[j]-(long)a[k]-(long)a[l])){
+                            first=(int)((long)tar-(long)a[j]-(long)a[k]-(long)a[l]);
+                        }else{
+                            continue;
+                        }
+                        if((long)first+(long)a[j]+(long)a[k]+(long)a[l]==(long)tar){
                             List<Integer> tem = new ArrayList<>();
-                            tem.add(a[i]);
-                            tem.add(a[j]);
-                            tem.add(a[k]);
-                            tem.add(a[l]);
-                            int[] ar2 = new int[]{a[i], a[j], a[k], a[l]};
+                            
+                            int[] ar2 = new int[]{first, a[j], a[k], a[l]};
                             Arrays.sort(ar2);
+                            tem.add(ar2[0]);
+                            tem.add(ar2[1]);
+                            tem.add(ar2[2]);
+                            tem.add(ar2[3]);
                             String s = Arrays.toString(ar2);
                             if(set.contains(s)){
-                                break;
+                                continue;
                             }else{
                                 set.add(s);
                                 ar.add(tem);
@@ -25,8 +35,9 @@ class Solution {
                         }
                     }
                 }
+                map.add((long)a[j]);
             }
-        }
+        // }
         return ar;
     }
 }
