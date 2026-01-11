cdfec5de05d0f19bdbce285eac0bd9ffb3872db4
Sayak1203 <123932629+Sayak1203@users.noreply.github.com>
2026-01-11 17:01:12 +0530

    Time: 89 ms (26.47%) | Memory: 117.7 MB (37.04%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -3,31 +3,31 @@ class Solution {
         // System.out.print(i+"-("+cur+") ");
         if(i==a.length) return 0;
         int buyIdx=(buy?0:1);
+        if(tran<0){
+            return 0;
+        }
         if(dp[i][tran][buyIdx]!=-1) return dp[i][tran][buyIdx];
-        if(buy && tran>0){
+        if(buy){
             int notTake = solve(a, buy, tran, i+1, cur, dp);
-            int take = -a[i]+solve(a, !buy, tran-1, i+1, cur, dp);
-            dp[i][tran][buyIdx]= Math.max(take, notTake);
-            return dp[i][tran][buyIdx];
-        }else if(!buy){
-            int notTake = solve(a, buy, tran, i+1, cur, dp);
-            int take = a[i]+solve(a, !buy, tran, i+1, cur, dp);
+            int take = -a[i]+solve(a, !buy, tran, i+1, cur, dp);
             dp[i][tran][buyIdx]= Math.max(take, notTake);
             return dp[i][tran][buyIdx];
         }else{
-            dp[i][tran][buyIdx]= cur;
+            int notTake = solve(a, buy, tran, i+1, cur, dp);
+            int take = a[i]+solve(a, !buy, tran-1, i+1, cur, dp);
+            dp[i][tran][buyIdx]= Math.max(take, notTake);
             return dp[i][tran][buyIdx];
         }
     }
     public int maxProfit(int[] a) {
         int n = a.length;
-        int[][][] dp= new int[n][3][2];
+        int[][][] dp= new int[n][2][2];
         for(int[][] i: dp){
             for(int[] j: i){
                 Arrays.fill(j,-1);
             }
         }
-        int ans = solve(a, true, 2, 0, 0, dp);
+        int ans = solve(a, true, 1, 0, 0, dp);
         // for(int[][] il: dp){
         //     for(int[] j: il){
         //         System.out.print(Arrays.toString(j)+" ");
